57368428672bd5db5c657df129bbe5764c2df8a3
Achartk <150880067+Achartk@users.noreply.github.com>
2023-11-15 20:44:44 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -62,6 +62,22 @@ let soal = [
 let nama = prompt('Input nama: ')
 
 // Lengkapi code program yang belum selesai
+for (let i = 0; i < soal.length; i++) {
+    console.log('\nSoal ' + (i + 1) + ': ' + soal[i].q);
+    for (let j = 0; j < soal[i].o.length; j++) {
+        console.log(j + '. ' + soal[i].o[j]);
+    }
+    let jawaban = prompt('Jawaban Anda (masukkan angka pilihan): ');
+    
+    if (jawaban == soal[i].a) {
+        console.log('Jawaban benar!\n');
+        score += point_per_soal;
+        jawaban_benar++;
+    } else {
+        console.log('Jawaban salah. Jawaban yang benar adalah: ' + soal[i].o[soal[i].a] + '\n');
+        jawaban_salah++;
+    }
+}
 
 console.log("Kuis selesai")
 console.log("\n")
@@ -70,6 +86,3 @@ console.log('Nama\t\t:',nama)
 console.log('Score\t\t:',score)
 console.log('Jawaban benar\t:',jawaban_benar)
 console.log('Jawaban salah\t:',jawaban_salah)
-
-
-
